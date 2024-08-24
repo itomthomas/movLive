4abaed0578c2af1e1bcfe69723d52f0336f24066
TomT <69982935+itomthomas@users.noreply.github.com>
2024-08-24 16:43:31 +0530

    PWA configured with sw basic.

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,5 +275,12 @@
       mirror: false
     })
   });
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(reg => console.log('Service Worker registered!', reg))
+      .catch(err => console.error('Service Worker registration failed:', err));
+  }
+  
 
 })()
